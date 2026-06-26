e2e57f0276c41bc75b2d051fec6cd4ddf31a27c6
Azhika <azhika2512@gmail.com>
2026-06-26 19:51:29 +0530

    Add three modules.v for module implementation

---
+++ 03-module/three modules.v
@@ -0,0 +1,7 @@
+top _module ( input d , input clk, output q);
+wires d1,d2;
+my_diff diff1(.clk(clk),.d(d),.q(d1);
+my_diffdiff2(.clk(clk),.d(d1),.q(d2)):
+my_diff diff3(.cllk(clk),.d(d2),.q(q));
+endmodule 
+           
